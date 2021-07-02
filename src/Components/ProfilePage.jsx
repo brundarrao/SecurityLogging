d329c2ca2bccbba260cf1434a0790fc6a0a299bf
Brunda Rao <brunda.r.1701@gmail.com>
2021-07-02 13:49:54 +0530

    Updating Log Info in browser

--- src/Components/ProfilePage.jsx
+++ src/Components/ProfilePage.jsx
@@ -4,6 +4,7 @@ import {auth} from "../firebase";
 import '../Styles/Components/button.css';
 
 class App extends Component {
+    counter = 0;
     constructor(props) {
         super(props);
 
@@ -12,7 +13,9 @@ class App extends Component {
                 accessToken: "fa02e1db2f884ef8a7607ab56413efc1",
                 captureUncaught: true,
                 captureUnhandledRejections: true,
-            })
+            }),
+            counter: 1,
+            counter2 : 1
         }
         //  this.props={
         //   user,
@@ -23,11 +26,13 @@ class App extends Component {
         this.logInfo = this.logInfo.bind(this);
         this.throwError = this.throwError.bind(this);
         this.logInfo2 = this.logInfo2.bind(this);
+       
     }
+    
     render() {
 
         const {
-            user
+            user, 
         } = this.props;
 
         return (
@@ -41,20 +46,55 @@ class App extends Component {
                         <button className="btn btn-prim" id='rollbar-info-2' onClick={ this.logInfo2 }>{user.displayName}</button>
                         <button className="btn btn-prim" id='throw-error' onClick={ this.throwError }>Error Test</button>
                         <p>Hello, {user.displayName}</p>
+                        <p id = 'pod'></p>
+                        <p id = 'pod2'></p>
                     </React.Fragment>
                 </header>
                 <button className = "w-full py-3 bg-pink-600 mt-4 text-white" onClick = {() => {auth.signOut(); this.logOut()}}>Sign out</button>
             </div>
         );
     }
+    html1(){
+       // <h1>"Update logs was clicked"</h1>;
+       //window.print("hbhjb")
+       
+
+       //event handler
+           //counter++;
+           //alert('Number of clicks:' + counter);
+       var bb = document.getElementById("pod");
+       this.setState({ counter: this.state.counter + 1 });
+       const curDate = new Date(Date.now());
+       const dateStr = curDate.toLocaleDateString('en-UK');
+       const timeStr = curDate.toLocaleTimeString('en-UK', {hour12: true});
+       bb.innerHTML= `Update Logs was clicked ${this.state.counter} times, last clicked on ${dateStr} at ${timeStr}`;
+    }
+
+    html2(){
+        // <h1>"Update logs was clicked"</h1>;
+        //window.print("hbhjb")
+        
+ 
+        //event handler
+            //counter++;
+            //alert('Number of clicks:' + counter);
+        var bb = document.getElementById("pod2");
+        this.setState({ counter2: this.state.counter2 + 1 });
+        const curDate = new Date(Date.now());
+        const dateStr = curDate.toLocaleDateString('en-UK');
+        const timeStr = curDate.toLocaleTimeString('en-UK', {hour12: true});
+        bb.innerHTML= `${this.props.user.displayName} was clicked ${this.state.counter2} times, last clicked on ${dateStr} at ${timeStr}`;
+     }
     logInfo() {
         // Example log event using the rollbar object.
         this.state.rollbar.info('Update Logs was clicked');
+        this.html1()
     }
 
     logInfo2(){
         // Second Example log event using the rollbar object.
         this.state.rollbar.info(this.props.user.displayName + ' was clicked')
+        this.html2()
     }
 
     throwError() {
